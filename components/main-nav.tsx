e1d51ae95a5de4e8eeee168895bfd55ff2305fad
dangpham <dangpham@pop-os.localdomain>
2024-04-15 17:40:13 +0700

    header

---
+++ components/main-nav.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import Link from "next/link"
+import { Icons } from "./icon"
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { usePathname } from "next/navigation"
+
+export function MainNav() {
+    const pathName = usePathname()
+    return <nav className="flex items-center space-x-4 lg:space-x-6">
+        <Link href="/" className="mr-6 flex items-center space-x-2">
+            <Icons.Logo className="h-6 w-6" />
+            <span className="font-bold">{siteConfig.name}</span>
+        </Link>
+        <Link href="/blog" className={cn("text-sm font-medium transition-colors hover:text-primary", pathName === "/blog" ? "text-foreground": "text-foreground/60")}>
+            Blog
+        </Link>
+        <Link href="/about" className={cn("text-sm font-medium transition-colors hover:text-primary", pathName === "/blog" ? "text-foreground": "text-foreground/60")}>
+            About me
+        </Link>
+    </nav>
+}
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "Damiens Blog",
+    url: "https://example.com",
+    description: "This is a description",
+    author: "Dang Pham",
+    links: {
+        "github": "https://github.com/dangpham205",
+        "linkedin": "https://www.linkedin.com/in/dang-pham-96a451234/"
+    }
+}
+
+export type SiteConfig = typeof siteConfig
