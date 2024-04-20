2e5db8689a87c7f97326ea015eff83be7634e610
dangpham205 <haidawng205@gmail.com>
2024-04-20 12:27:29 +0700

    update

--- velite.config.ts
+++ velite.config.ts
@@ -1,43 +1,54 @@
-import { defineCollection, defineConfig, s } from 'velite'
+import { defineConfig, defineCollection, s } from "velite";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-// `s` is extended from Zod with some custom schemas,
-// you can also import re-exported `z` from `velite` if you don't need these extension schemas.
-
-const computedFields = <T extends {slug: string}>(data: T) => ({
-    ...data,
-    slugAsParams: data.slug.split("/").slice(1).join("/")
-})
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
 
 const posts = defineCollection({
-    name: 'Post', // collection type name
-    pattern: 'blog/**/*.mdx', // content files glob pattern
-    schema: s
+  name: "Post",
+  pattern: "blog/**/*.mdx",
+  schema: s
     .object({
-        slug: s.path(),
-        title: s.string().max(99),
-        description: s.string().max(999).optional(),
-        date: s.isodate(),
-        published: s.boolean().default(true),
-        body: s.mdx(), // transform markdown to html
-        cover: s.image().optional(), // input image relative path, output image object with blurImage.
-        video: s.file().optional(), // input file relative path, output file public path.
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      tags: s.array(s.string()).optional(),
+      body: s.mdx(),
     })
-    .transform(computedFields)
-    // more additional fields (computed fields)
-})
+    .transform(computedFields),
+});
 
 export default defineConfig({
-    root: "content",
-    output: {
-        data: ".velite",
-        assets: "public/static",
-        base: "/static/",
-        name: "[name]-[hash:6].[ext]",
-        clean: true
-    },
-    collections: {posts},
-    mdx: {
-        rehypePlugins: [],
-        remarkPlugins: []
-    }
-})
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: { posts },
+  mdx: {
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, { theme: "github-dark" }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+    remarkPlugins: [],
+  },
+});
